652580f3f87cb4cc5e81afb0dab65c4ae93b0695
wan-elmus <elmuswangudi@gmail.com>
2023-02-07 18:58:52 +0300

    Modified code to use malloc() and added new files

---
+++ mult.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+
+#define N 1024
+
+double **A;
+double **B;
+double **C;
+
+void init_matrices()
+{
+    A = malloc(N * sizeof(double *));
+    B = malloc(N * sizeof(double *));
+    C = malloc(N * sizeof(double *));
+    for (int i = 0; i < N; i++)
+    {
+        A[i] = malloc(N * sizeof(double));
+        B[i] = malloc(N * sizeof(double));
+        C[i] = malloc(N * sizeof(double));
+        for (int j = 0; j < N; j++)
+        {
+            A[i][j] = (double)rand() / RAND_MAX;
+            B[i][j] = (double)rand() / RAND_MAX;
+            C[i][j] = 0.0;
+        }
+    }
+}
+
+void matrix_mult()
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+        {
+            for (int k = 0; k < N; k++)
+            {
+                C[i][j] += A[i][k] * B[k][j];
+            }
+        }
+    }
+}
+
+int main()
+{
+    struct timeval start, end;
+    init_matrices();
+    gettimeofday(&start, NULL);
+    matrix_mult();
+    gettimeofday(&end, NULL);
+    double elapsed_time = (end.tv_sec - start.tv_sec) * 1000000.0 + (end.tv_usec - start.tv_usec);
+    printf("Elapsed time: %lf microseconds\n", elapsed_time);
+    for (int i = 0; i < N; i++)
+    {
+        free(A[i]);
+        free(B[i]);
+        free(C[i]);
+    }
+    free(A);
+    free(B);
+    free(C);
+    return 0;
+}
