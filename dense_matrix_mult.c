c324a45ae156ef30a364ef551aeb565828c84380
wan-elmus <elmuswangudi@gmail.com>
2023-02-08 03:00:18 +0300

    simple mods

--- dense_matrix_mult.c
+++ dense_matrix_mult.c
@@ -1,3 +1,4 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/time.h>
@@ -6,18 +7,12 @@
 
 double A[N][N], B[N][N], C[N][N];
 
-<<<<<<< HEAD
-void init_matrices() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-=======
 void init_matrices()
 {
     for (int i = 0; i < N; i++)
     {
         for (int j = 0; j < N; j++)
         {
->>>>>>> 425a639256ea2de31346dbcad81a3fbc8f3a354e
             A[i][j] = (double)rand() / RAND_MAX;
             B[i][j] = (double)rand() / RAND_MAX;
             C[i][j] = 0.0;
@@ -25,12 +20,6 @@ void init_matrices()
     }
 }
 
-<<<<<<< HEAD
-void matrix_mult() {
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < N; j++) {
-            for (int k = 0; k < N; k++) {
-=======
 void matrix_mult()
 {
     for (int i = 0; i < N; i++)
@@ -39,19 +28,14 @@ void matrix_mult()
         {
             for (int k = 0; k < N; k++)
             {
->>>>>>> 425a639256ea2de31346dbcad81a3fbc8f3a354e
                 C[i][j] += A[i][k] * B[k][j];
             }
         }
     }
 }
 
-<<<<<<< HEAD
-int main() {
-=======
 int main()
 {
->>>>>>> 425a639256ea2de31346dbcad81a3fbc8f3a354e
     struct timeval start, end;
     init_matrices();
     gettimeofday(&start, NULL);
